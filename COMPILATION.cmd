f56cf54bb8c1577c78e4c9061af543fa750e1d58
iurybakov <iu.rybakov@gmail.com>
2017-02-02 16:56:02 +0300

    modifed

--- COMPILATION.cmd
+++ COMPILATION.cmd
@@ -3,15 +3,12 @@ set manif.mf = %CD%
 @echo Class-Path: . >> manif.mf
 @echo Main-Class: parseroracle2tier.ParserOracle2Tier >> manif.mf
 @echo OFF
-javac *.java
-mkdir parseroracle2tier
-move *.class parseroracle2tier
+javac parseroracle2tier\\*.java
+del parseroracle2tier\\*.java
 jar -cmf manif.mf Parser.jar *
 rmdir /S /Q parseroracle2tier
+rmdir /S /Q res
 rmdir /S /Q .git
 del README.md
 del manif.mf
-del *.java
 del COMPILATION.cmd
-
-
