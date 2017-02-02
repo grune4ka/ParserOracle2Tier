9554211601b626d02f46b3ef7d03466b5a45adea
iurybakov <iu.rybakov@gmail.com>
2017-02-02 17:19:28 +0300

    modifed

--- COMPILATION.cmd
+++ COMPILATION.cmd
@@ -1,14 +1,13 @@
-set manif.mf = %CD%
-@echo Manifest-Version: 1.0 > manif.mf
-@echo Class-Path: . >> manif.mf
-@echo Main-Class: parseroracle2tier.ParserOracle2Tier >> manif.mf
+set MANIFEST.MF = %CD%
+@echo Manifest-Version: 1.0 > MANIFEST.MF
+@echo Class-Path: . >> MANIFEST.MF
+@echo Main-Class: parseroracle2tier.ParserOracle2Tier >> MANIFEST.MF
 @echo OFF
 javac parseroracle2tier\\*.java
 del parseroracle2tier\\*.java
-jar -cmf manif.mf Parser.jar *
+jar -cmf MANIFEST.MF Parser.jar res parseroracle2tier
 rmdir /S /Q parseroracle2tier
 rmdir /S /Q res
 rmdir /S /Q .git
-del README.md
-del manif.mf
+del MANIFEST.MF
 del COMPILATION.cmd
